c4f67f0dc22f5683f80cb02880a6d9b163c4456c
whispora <whispora@gmail.com>
2025-04-07 22:54:48 +0800

    fix: update core

--- packages/core/lib/log/index.ts
+++ packages/core/lib/log/index.ts
@@ -1,17 +1,27 @@
 import chalk from 'chalk';
 import ora, { Color } from 'ora';
+chalk.level = 3;
 
 type logType = "info" | "success" | "warn" | "error"
 
+const colorMap: Record<string, (text: string) => string> = {
+    error: chalk.red,
+    success: chalk.green,
+    info: chalk.blue,
+    warn: chalk.yellow
+    // 添加更多颜色
+};
 
 // 日志
-export function log(msg: string, type: logType = "info") {
-    console.log(chalk[type as Color](msg));
+export function log(msg: string, type: logType) {
+
+    const colorFn = colorMap[type];
+    console.log(colorFn(msg));
 }
 
 
 // 加载
-export function loading(msg: string) {
+export function logLoading(msg: string) {
     const spinner = ora(msg).start();
     return spinner;
 }
